0d116a87d030b3db9bbd5ff0a1b4309d19a1a6dd
Olexander <alex.pilyavskiy03@gmail.com>
2024-03-17 19:34:25 +0200

    create task 1

---
+++ objects&array/task01.js
@@ -0,0 +1,52 @@
+/*1. Напишите функцию, которая принимает массив объектов и имя свойства, 
+а возвращает новый массив объектов,
+ где указанное свойство соответствует заданному условию*/
+
+// const people = [
+//   {
+//     name: "Oleksandr",
+//     age: 22,
+//     budget: 8000,
+//   },
+//   {
+//     name: "Dmytro",
+//     age: 33,
+//     budget: 50000,
+//   },
+//   {
+//     name: "Ekaterina",
+//     age: 29,
+//     budget: 20000,
+//   },
+//   {
+//     name: "Elena",
+//     age: 17,
+//     budget: 4000,
+//   },
+//   {
+//     name: "Alina",
+//     age: 16,
+//     budget: 5000,
+//   },
+//   {
+//     name: "Vlad",
+//     age: 20,
+//     budget: 3000,
+//   },
+// ];
+
+// const filteredPeople = people.filter((person) => {
+//   return person.age >= 18;
+// });
+
+// console.log(filteredPeople);
+//========================================================================================================================================================
+// let newPeople = [];
+
+// for (let i = 0; i < people.length; i++) {
+//   if (people[i].age >= 18) {
+//     newPeople.push(people[i]);
+//   }
+// }
+
+// console.log(newPeople);
