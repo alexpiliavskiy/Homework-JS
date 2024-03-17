9ac526ca50b6efbb7bb04bf7f922e5eba6df0c53
Olexander <alex.pilyavskiy03@gmail.com>
2024-03-17 19:44:31 +0200

    create task 2

---
+++ objects&array/task02.js
@@ -0,0 +1,47 @@
+/*Создайте функцию, которая сортирует массив объектов
+ по указанному свойству (возрастание или убывание).*/
+
+// const people = [
+//   {
+//     name: "Oleksandr",
+//     age: 22,
+//     budget: 8000,
+//   },
+//   {
+//     name: "Dmytro",
+//     age: 33,
+//     budget: 50000,
+//   },
+//   {
+//     name: "Ekaterina",
+//     age: 29,
+//     budget: 20000,
+//   },
+//   {
+//     name: "Elena",
+//     age: 17,
+//     budget: 4000,
+//   },
+//   {
+//     name: "Alina",
+//     age: 16,
+//     budget: 5000,
+//   },
+//   {
+//     name: "Vlad",
+//     age: 20,
+//     budget: 3000,
+//   },
+// ];
+
+// const sortedPeopleAge = people.sort((a, b) => {
+//   return a.age > b.age ? 1 : -1;
+// });
+
+// console.log(sortedPeopleAge);
+//========================================================================================================================================================
+// const sortedPeopleAge = people.sort((a, b) => {
+//   return b.age > a.age ? 1 : -1;
+// });
+
+// console.log(sortedPeopleAge);
